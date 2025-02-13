595cd6b9708d76ba9cdf0258c94beccf4b73fdaa
Bill Liu <bl698@drexel.edu>
2025-02-12 23:55:17 -0500

    Completed assignment

--- 3-ShellP1/starter/dsh_cli.c
+++ 3-ShellP1/starter/dsh_cli.c
@@ -46,10 +46,59 @@
  */
 int main()
 {
-    char *cmd_buff;
+    char cmd_buff[SH_CMD_MAX];
     int rc = 0;
     command_list_t clist;
 
-    printf(M_NOT_IMPL);
-    exit(EXIT_NOT_IMPL);
+    
+
+    while (1)
+    {
+        printf("%s", SH_PROMPT);
+        if (fgets(cmd_buff, SH_CMD_MAX, stdin) == NULL)
+        {
+            printf("\n");
+            break; // Handle EOF (Ctrl+D)
+        }
+
+        // Remove trailing newline character
+        cmd_buff[strcspn(cmd_buff, "\n")] = '\0';
+
+        // Check if the input is empty
+        if (strlen(cmd_buff) == 0)
+        {
+            printf(CMD_WARN_NO_CMD);
+            continue;
+        }
+
+        // Check if the command is "exit"
+        if (strcmp(cmd_buff, EXIT_CMD) == 0)
+        {
+            printf("Exiting shell\n");
+            break;
+        }
+
+        // Parse the command using parse_command_list
+        rc = build_cmd_list(cmd_buff, &clist);
+        if (rc == ERR_TOO_MANY_COMMANDS)
+        {
+            printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
+            continue;
+        }
+        else if (rc == OK)
+        {
+            printf(CMD_OK_HEADER, clist.num);
+            for (int i = 0; i < clist.num; i++)
+            {
+                printf("<%d> %s", i+1, clist.commands[i].exe);
+                if (strlen(clist.commands[i].args) > 0)
+                {
+                    printf(" [%s]", clist.commands[i].args);
+                }
+                printf("\n");
+            }
+        }
+    }
+    
+    exit(0);
 }
--- 3-ShellP1/starter/dshlib.c
+++ 3-ShellP1/starter/dshlib.c
@@ -32,8 +32,63 @@
  *  Standard Library Functions You Might Want To Consider Using
  *      memset(), strcmp(), strcpy(), strtok(), strlen(), strchr()
  */
-int build_cmd_list(char *cmd_line, command_list_t *clist)
-{
-    printf(M_NOT_IMPL);
-    return EXIT_NOT_IMPL;
+ int build_cmd_list(char *cmd_line, command_list_t *clist) {
+    memset(clist, 0, sizeof(command_list_t)); 
+
+    // printf("BUILD COMMAND LIST: RECEIVED: \"%s\"\n", cmd_line);
+
+    char *save_ptr1, *save_ptr2;
+    char *cmd_ptr = strtok_r(cmd_line, "|", &save_ptr1); // split by "|"
+    int cmd_count = 0;
+
+    while (cmd_ptr != NULL) {
+        // trim
+        while (isspace((unsigned char)*cmd_ptr)) cmd_ptr++;
+        char *end = cmd_ptr + strlen(cmd_ptr) - 1;
+        while (end > cmd_ptr && isspace((unsigned char)*end)) {
+            *end = '\0';
+            end--;
+        }
+
+        // printf("BUILD COMMAND LIST PARSED: %d: \"%s\"\n", cmd_count + 1, cmd_ptr);
+
+        if (cmd_count >= CMD_MAX) {
+            return ERR_TOO_MANY_COMMANDS;
+        }
+
+        command_t *cmd = &clist->commands[cmd_count];
+
+        // extract command name
+        char *arg_ptr = strtok_r(cmd_ptr, " ", &save_ptr2);
+        if (arg_ptr == NULL || strlen(arg_ptr) >= EXE_MAX) {
+            return ERR_CMD_OR_ARGS_TOO_BIG;
+        }
+        strncpy(cmd->exe, arg_ptr, EXE_MAX);
+        cmd->exe[EXE_MAX - 1] = '\0';
+
+        // extract arguments
+        cmd->args[0] = '\0';
+        int args_len = 0;
+
+        while ((arg_ptr = strtok_r(NULL, " ", &save_ptr2)) != NULL) {
+            if (args_len + strlen(arg_ptr) + 1 >= ARG_MAX) {
+                return ERR_CMD_OR_ARGS_TOO_BIG;
+            }
+            if (args_len > 0) {
+                strcat(cmd->args, " ");
+                args_len++;
+            }
+            strcat(cmd->args, arg_ptr);
+            args_len += strlen(arg_ptr);
+        }
+
+        cmd_count++;
+        cmd_ptr = strtok_r(NULL, "|", &save_ptr1); // go to next command
+    }
+
+    clist->num = cmd_count;
+    return OK;
 }
+
+
+
