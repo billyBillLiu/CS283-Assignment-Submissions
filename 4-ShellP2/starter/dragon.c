d9e2625a34866a9ab9c1bedc01485b2181039e25
Bill Liu <bl698@drexel.edu>
2025-02-20 00:00:38 -0500

    completed code

--- 4-ShellP2/starter/dragon.c
+++ 4-ShellP2/starter/dragon.c
@@ -3,4 +3,12 @@
 // EXTRA CREDIT - print the drexel dragon from the readme.md
 extern void print_dragon(){
   // TODO implement 
+  FILE *file = fopen("dragon.txt", "r");
+            char line[256];
+            while (fgets(line, sizeof(line), file))
+            {
+                printf("%s", line);
+            }
+            printf("\n");
+            fclose(file);
 }
--- 4-ShellP2/starter/dshlib.c
+++ 4-ShellP2/starter/dshlib.c
@@ -51,21 +51,124 @@
  *  Standard Library Functions You Might Want To Consider Using (assignment 2+)
  *      fork(), execvp(), exit(), chdir()
  */
-int exec_local_cmd_loop()
-{
-    char *cmd_buff;
-    int rc = 0;
-    cmd_buff_t cmd;
-
-    // TODO IMPLEMENT MAIN LOOP
-
-    // TODO IMPLEMENT parsing input to cmd_buff_t *cmd_buff
-
-    // TODO IMPLEMENT if built-in command, execute builtin logic for exit, cd (extra credit: dragon)
-    // the cd command should chdir to the provided directory; if no directory is provided, do nothing
-
-    // TODO IMPLEMENT if not built-in command, fork/exec as an external command
-    // for example, if the user input is "ls -l", you would fork/exec the command "ls" with the arg "-l"
-
-    return OK;
-}
+ int exec_local_cmd_loop()
+ {
+     char *cmd_buff = malloc(SH_CMD_MAX);
+     int rc = 0;
+     cmd_buff_t cmd = {0};
+ 
+     while (1)
+     {
+         printf("%s", SH_PROMPT);
+         if (fgets(cmd_buff, SH_CMD_MAX, stdin) == NULL)
+         {
+             printf("\n");
+             break;
+         }
+ 
+         // Trim leading and trailing spaces
+         char *start = cmd_buff;
+         while (isspace((unsigned char)*start))
+             start++;
+ 
+         char *end = start + strlen(start) - 1;
+         while (end > start && isspace((unsigned char)*end))
+             *end-- = '\0';
+ 
+         if (*start == '\0')
+         {
+             printf(CMD_WARN_NO_CMD);
+             continue;
+         }
+ 
+         // Handle quoted arguments and space elimination
+         cmd.argc = 0;
+         cmd._cmd_buffer = start;
+         bool in_quotes = false;
+         char *token = strtok(start, " ");
+         
+         while (token && cmd.argc < CMD_ARGV_MAX - 1)
+         {
+             if (token[0] == '"')
+             {
+                 in_quotes = true;
+                 cmd.argv[cmd.argc++] = token + 1;
+             }
+             else if (in_quotes)
+             {
+                 strcat(cmd.argv[cmd.argc - 1], " ");
+                 strcat(cmd.argv[cmd.argc - 1], token);
+                 if (token[strlen(token) - 1] == '"')
+                 {
+                     in_quotes = false;
+                     token[strlen(token) - 1] = '\0';
+                 }
+             }
+             else
+             {
+                 cmd.argv[cmd.argc++] = token;
+             }
+             token = strtok(NULL, " ");
+         }
+         cmd.argv[cmd.argc] = NULL;
+ 
+         if (cmd.argc == 0)
+         {
+             printf(CMD_WARN_NO_CMD);
+             continue;
+         }
+ 
+         // Built-in command handling
+         if (strcmp(cmd.argv[0], EXIT_CMD) == 0)
+         {
+             printf("Exiting shell\n");
+             break;
+         }
+         else if (strcmp(cmd.argv[0], "dragon") == 0)
+         {
+             print_dragon();
+             continue;
+         }
+         else if (strcmp(cmd.argv[0], "cd") == 0)
+         {
+             if (cmd.argc == 1)
+             {
+                 // Do nothing when no argument is provided
+             }
+             else if (cmd.argc > 1)
+             {
+                 if (chdir(cmd.argv[1]) != 0)
+                 {
+                     perror("cd failed");
+                 }
+             }
+             continue;
+         }
+ 
+         // Execute external command
+         pid_t pid = fork();
+         if (pid == 0)
+         {
+             execvp(cmd.argv[0], cmd.argv);
+             perror(CMD_ERR_EXECUTE);
+             exit(ERR_EXEC_CMD);
+         }
+         else if (pid > 0)
+         {
+             int status;
+             waitpid(pid, &status, 0);
+             if (WIFEXITED(status))
+             {
+                 int exit_code = WEXITSTATUS(status);
+                 // printf("Process exited with status %d\n", exit_code);
+             }
+         }
+         else
+         {
+             perror("fork failed");
+         }
+     }
+ 
+     free(cmd_buff);
+     return OK;
+ }
